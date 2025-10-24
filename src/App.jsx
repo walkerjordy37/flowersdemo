7bc0838b8a4dc66797a7eb6cab119cdb4c22f7e7
m2jorientation <m2jorientation@gmail.com>
2025-10-24 05:47:23 +0200

    Mise à jour

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ImageUploader from './components/ImageUploader.jsx'
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>Identificateur de fleurs 🌸</h1>
       <ImageUploader />
     </div>
   )
